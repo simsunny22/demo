ecaa0585cf85d2c4dc4d84a59d39e157e596b098
free_spring <free_spring@freespringdeAir.lan>
2018-08-16 07:42:35 +0800

    fix format

--- c++/right_val.cc
+++ c++/right_val.cc
@@ -5,18 +5,18 @@ using namespace std;
 
 // rigit and left value
 void test_lr_value() {
-    int   i =1 ;
-	int&  l_ref = i;
-	int&& r_ref = 2;
+    int   i = 1 ;
+    int&  l_ref = i;
+    int&& r_ref = 2;
     //int&& r_ref = i; //compile error(because i is l_value)
-	std::cout << "l_ref:" << l_ref << std::endl;
-	std::cout << "r_ref:" << r_ref << std::endl;
+    std::cout << "l_ref:" << l_ref << std::endl;
+    std::cout << "r_ref:" << r_ref << std::endl;
     std::cout << "**********************" << std::endl;
 }
 
 // rigit value
 void processValue(int& a) {
-	cout << "process_left_val" << endl; 
+    cout << "process_left_val" << endl; 
 }
 
 void processValue(int&& a) { 
@@ -24,19 +24,19 @@ void processValue(int&& a) {
 }
 
 void forward_value(int& val) {
-	cout << "forward_left_val"  << endl;
+    cout << "forward_left_val"  << endl;
     processValue(val); 
 }
 
 void forward_value(int&& val) {
-	cout << "forward_right_val" << endl;
+    cout << "forward_right_val" << endl;
     processValue(val); 
 }
 
 void test_right_value() {
-	int i = 1;
-	forward_value(i);
-	forward_value(2);
+    int i = 1;
+    forward_value(i);
+    forward_value(2);
     std::cout << "**********************" << std::endl;
 }
 
@@ -56,6 +56,6 @@ void test_forward_value(){
 
 int main(){
     test_lr_value();
-	test_right_value();
+    test_right_value();
     test_forward_value();
 }
