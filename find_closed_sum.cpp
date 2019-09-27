a2ffd8d037365c176909a9ce23874e3c9861dcbc
武昭01 <zhao.wu01@ucarinc.com>
2019-09-27 09:47:53 +0800

    fix struct name

--- find_closed_sum.cpp
+++ find_closed_sum.cpp
@@ -1,58 +1,75 @@
 #include <iostream>
 #include <algorithm>
 
-int find_close_num(int *a, int len, int sum);
+
+class Result {
+public:
+	Result(int i, int j) {
+		set(i, j);
+	}
+
+	void set(int i, int j) {
+		this->i = i;
+		this->j = j;
+	}
+	void print(int num) {
+		std::cout << "i:" << this->i << ",j:" << this->j << std::endl;
+		std::cout << "sum:" << this->i+ this->j << ",num:" << num << std::endl;
+	}
+private:
+	int i;
+	int j;
+};
+
+Result find_close_num(int *a, int len, int sum);
 
 int main () {
 	int a[] = {5,43,12,43,32,13,87,90,15,25,9};
 
-
 	std::cout << std::endl;
-	find_close_num(a, 11, 60);
+	Result result = find_close_num(a, 11, 60);
+	result.print(60);
 }                                               
 
-
-int find_close_num(int *a, int len, int sum) {
+void print_list(int *a, int len) {
 	for(int i= 0; i < 11; i++) {
 		std::cout << a[i] << " "; 
 	}
 	std::cout << std::endl;
-	std::cout << "sum:" << sum << std::endl;
+
+}
+
+Result find_close_num(int *a, int len, int num) {
+	//sort array
+	print_list(a, len);
 	std::sort(a, a+11);
-	for(int i= 0; i < 11; i++) {
-		std::cout << a[i] << " "; 
-	}
-	std::cout << std::endl;
+	print_list(a, len);
 
-
+	Result result(0,0);
 	int i = 0, j = len-1;
-	int min = a[i], max = a[j];
-
 	int current_abs = 0;
-	int last_abs = abs(2*max-sum) > abs(2*min-sum) ? 2*max : 2*min;
+	int min = a[i], max = a[j];
+	int last_abs = abs(2*max-num) > abs(2*min-num) ? 2*max : 2*min;
 
-	int ret_i, ret_j;
 	while(i < j) {
 		int _i = i, _j = j;
 		int _sum = a[i] + a[j];
 
-		if (_sum > sum) {
+		if (_sum > num) {
 			j--;	
-		} else if (_sum < sum) {
+		} else if (_sum < num) {
 			i++;
 		} else {
-			ret_i = _i, ret_j = _j;
+			result.set(a[_i], a[_j]);
 			break;
 		}
 
-		current_abs = abs(_sum - sum);
+		current_abs = abs(_sum - num);
+		//if current_abs < last_abs, means more close, so record result
 		if (current_abs < last_abs) {
 			last_abs = current_abs;
-			ret_i = _i;
-			ret_j = _j;
+			result.set(a[_i], a[_j]);
 		}
 	}
-	std::cout << "res:" << a[ret_i] << std::endl;
-	std::cout << "res:" << a[ret_j] << std::endl;
-	return 0;
+	return result;
 }
